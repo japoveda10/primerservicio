6929dd6e1dca4fd6ad5bfdccdcb7564e605eec10
japoveda10 <ja.poveda10@uniandes.edu.co>
2017-08-22 21:53:06 -0500

    Small logo and footer fix

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+  $(".navbar a, footer a[href='#miPagina']").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== ""){
       // Prevent default anchor click behavior
       event.preventDefault();
 
@@ -19,16 +19,5 @@ $(document).ready(function(){
         window.location.hash = hash;
       });
     } // End if
-  });
-
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-    });
   });
 })
